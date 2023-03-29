ab818e61e5cb7da62b082c921af2699c8aef4690
binbashz <5732@holbertonstudnets.com>
2023-03-28 17:37:43 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,39 @@
+nclude "main.h"
+
+/**
+ * _printf - Writes output to stdout
+ * @format: String to print
+ *
+ * Return: Number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0;
+	unsigned int string_lenght = 0;
+	
+	va_list args;
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+
+	for ( i = 0; format[i] = '\0'; i++)
+		{
+	if (format[i] == '%')
+	{
+		if (format[i + 1] == '\0')
+	string_lenght += get_function_print(aegs, format [i + 1]);
+}
+	else 
+	{
+	_putchar(format[i];
+	
+	string_lenght++;
+}
+
+	}
+	va_end(args);		
+	return (-1);
+}
