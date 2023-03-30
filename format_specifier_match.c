bf68ec0f8a3fbe7cde24b0fa2e4337d7e538d9e1
binbashz <5732@holbertonstudnets.com>
2023-03-30 10:33:06 -0700

    get_format

---
+++ format_specifier_match.c
@@ -0,0 +1,9 @@
+
+/**
+ * format _specifier_match - Selects correct function
+ * @args: Arguments
+ * @spec: Character corresponding to printf specifier (format[i + 1])
+ *
+ * Return: Pointer to selected function or length of string printed
+*/
+#include "main.h"
