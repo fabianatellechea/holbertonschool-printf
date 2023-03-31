e6c69c7708d1a4768559f0ce6dfe8b95dcd1e813
01101101 01100001 01110101 <124454895+binbashz@users.noreply.github.com>
2023-03-31 00:42:32 -0300

    Update format_specifier_match.c

--- format_specifier_match.c
+++ format_specifier_match.c
@@ -1,6 +1,6 @@
 
 /**
- * format _specifier_match - Selects correct function
+ * format_specifier_match - Selects correct function
  * @args: Arguments
  * @spec: Character corresponding to printf specifier (format[i + 1])
  *
@@ -8,9 +8,9 @@
 */
 #include "main.h"
 
-	int format _specifier_match(va_list args, char spec)
+	int format_specifier_match(va_list args, char spec)
 {
-	struc_print caracter[] = {
+	specifier_t caracter[] = {
 		{"c", },
 		{"%", },
 		{"s", },
