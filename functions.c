47da2b23647504422dc50b66d96d58ebaad595d3
fabianatellechea <fabianatellechea@gmail.com>
2023-04-02 15:02:54 -0700

    comments to print i func

--- functions.c
+++ functions.c
@@ -61,9 +61,9 @@ int print_i(va_list list)
 {
 	int i = 0;
 	int n = va_arg(list, int);
-	int div = 1;
-	int d;
-	unsigned int aux;
+	int div = 1; /* divide the integer into digits */
+	int d; /*store the current digit*/
+	unsigned int aux; /*handle the absolute value of the integer */
 
 	if (n < 0)
 	{
@@ -76,6 +76,7 @@ int print_i(va_list list)
 		aux = n;
 	}
 
+	/* Determine the divisor needed to get the first digit */
 	while (aux / div >= 10)
 	{
 		div = div * 10;
@@ -83,10 +84,10 @@ int print_i(va_list list)
 
 	while (div > 0)
 	{
-		d = aux / div;
+		d = aux / div;/* Get the current digit by dividing the aux variable by the divisor */
 		_putchar(d + '0');
-		aux %= div;
-		div /= 10;
+		aux %= div;/* Update the auxiliary value by removing the current digit */
+		div /= 10;/* Update the divisor by dividing it by 10 to get the next digit */
 		i++;
 	}
 
